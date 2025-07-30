95e2b22f4f5aaa3bb4be179dc465fdf5b79901e1
Van1841 <119294875+Van1841@users.noreply.github.com>
2025-07-30 23:50:53 +0530

    ✨ Updated resume link and removed underline styling

--- src/Components/navigation_bar/HEro/hero.jsx
+++ src/Components/navigation_bar/HEro/hero.jsx
@@ -11,7 +11,9 @@ const hero = () => {
             <p>Hey, I’m a Computer Science undergraduate at VIT Bhopal University with an insatiable appetite for innovation and a meticulous eye for detail. My academic journey is a confluence of theoretical depth and hands-on pragmatism. </p>
             <div className='hero-action'>
                 <div className='hero-connect'><AnchorLink className="anchor-link" offset ={50} href='#contact'>Connect with me</AnchorLink></div>
-                <div className='hero-resume'>My resume</div>
+                {/* <div className='hero-resume'>My resume</div> */}
+                <a href="/VANSHIKA_BHOJANI_RESUME.pdf" className="hero-resume" target="_blank" rel="noopener noreferrer">My Resume</a>
+
             </div>
         </div>
     )
